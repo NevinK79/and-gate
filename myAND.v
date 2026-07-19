caa01cea47f565b8a950b56e656acaf8b90aa5cc
Nevin Kuttarappallil <nevinkuttara@gmail.com>
2026-07-18 23:05:28 -0500

    Add files via upload

---
+++ myAND.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/09/2026 11:12:41 PM
+// Design Name: 
+// Module Name: myAND
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+module myAND(
+    input a,
+    input b,
+    output out
+    );
+    
+    and a0 (out, a, b);
+endmodule
